2b296605f57edd94c8a79905f54accb67a608ea1
marcusklaesson <55803843+marcusklaesson@users.noreply.github.com>
2020-01-30 17:45:43 +0100

    fixed new design for headers

--- Labb2/ToDoTableViewController.m
+++ Labb2/ToDoTableViewController.m
@@ -11,48 +11,72 @@
 
 @interface ToDoTableViewController ()
 @property (nonatomic) NSMutableArray *todoTask;
-@property (nonatomic) int selectedRow;
+@property (nonatomic) NSMutableArray *importantTask;
+@property (nonatomic) NSMutableArray *completedTask;
+
 @end
 
 @implementation ToDoTableViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
+
     self.todoTask = [[NSMutableArray alloc] init];
-   
+    self.importantTask = [[NSMutableArray alloc] init];
+    self.completedTask = [[NSMutableArray alloc] init];
    
     [self loadData];
-    
-    
-    
     
     UILongPressGestureRecognizer* longPressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(onLongPress:)];
     [self.tableView addGestureRecognizer:longPressRecognizer];
     
-    
-    
+
 }
 
 -(void)saveData{
     
-    [[NSUserDefaults standardUserDefaults] setObject:self.todoTask forKey:@"TheKey"];
-      [[NSUserDefaults standardUserDefaults] synchronize];
-      
-      NSLog(@"SAVE: %@", self.todoTask);
+   /* for (int i = 0; i < self.completedTask.count; i++) {
+        NSString *key = [NSString stringWithFormat:@"%d", ((void)(@"completed %d"), i)]
+        [[[[NSUserDefaults standardUserDefaults] setObject:self.completedTask[i] forKey:((void)(@"completed %d"), i)] ] ];
+                     [[NSUserDefaults standardUserDefaults] synchronize];
+                     
+                     NSLog(@"SAVE TO COMPLETED: %@", self.completedTask);
+    }*/
     
-
+        [[NSUserDefaults standardUserDefaults] setObject:self.completedTask forKey:@"completed"];
+               [[NSUserDefaults standardUserDefaults] synchronize];
+               
+               NSLog(@"SAVE TO COMPLETED: %@", self.completedTask);
+  
+        [[NSUserDefaults standardUserDefaults] setObject:self.importantTask forKey:@"important"];
+                   [[NSUserDefaults standardUserDefaults] synchronize];
+                   
+                   NSLog(@"SAVE TO IMPORTANT : %@", self.importantTask);
+        
+        [[NSUserDefaults standardUserDefaults] setObject:self.todoTask forKey:@"todo"];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+            
+            NSLog(@"SAVE TO TODO: %@", self.todoTask);
     
-
+    
 }
 -(void)loadData{
     
-    self.todoTask = [[[NSUserDefaults standardUserDefaults] objectForKey:@"TheKey"] mutableCopy];
+        self.completedTask = [[[NSUserDefaults standardUserDefaults] objectForKey:@"completed"] mutableCopy];
+
+           NSLog(@"LOAD: %@", self.completedTask);
+        
+   
+        self.todoTask = [[[NSUserDefaults standardUserDefaults] objectForKey:@"important"] mutableCopy];
+
+           NSLog(@"LOAD: %@", self.importantTask);
+        
+
+    self.todoTask = [[[NSUserDefaults standardUserDefaults] objectForKey:@"todo"] mutableCopy];
 
        NSLog(@"LOAD: %@", self.todoTask);
     
-        
 }
 - (IBAction)addTodo:(id)sender {
     
@@ -64,14 +88,9 @@
         
         if(textField.text.length > 0){
             
-            
             NSMutableString *todo = [NSMutableString stringWithFormat:@"%@", textField.text];
             
-            
             [self.todoTask addObject:todo];
-            
-            
-            
             
             NSLog(@"todo clicks: %lu", (unsigned long)self.todoTask.count);
             
@@ -97,7 +116,6 @@
     [self presentViewController:alert animated:YES completion:nil];
     
 }
-
 -(void)onLongPress:(UILongPressGestureRecognizer*)pGesture {
 
 if (pGesture.state == UIGestureRecognizerStateEnded)
@@ -106,110 +124,186 @@ if (pGesture.state == UIGestureRecognizerStateEnded)
     CGPoint touchPoint = [pGesture locationInView:self.view];
     NSIndexPath* selectedCell = [tableView indexPathForRowAtPoint:touchPoint];
     
-    
     if (selectedCell != nil) {
         
         UIAlertController * alert = [UIAlertController alertControllerWithTitle:@"Task options" message:@"" preferredStyle: UIAlertControllerStyleAlert];
         
         UIAlertAction *prioAction = [UIAlertAction actionWithTitle:@"Prioritize" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
             
-            NSMutableString *todo = self.todoTask[selectedCell.row];
-        
             
-        
+            NSString *string = self.todoTask[selectedCell.row];
+            
+            
             [self.todoTask removeObjectAtIndex:selectedCell.row];
-            [self.todoTask insertObject:todo atIndex:0];
-            [tableView reloadData];
-            [self saveData];
+            [self.importantTask addObject:string];
             
+            
+        
+            [self.tableView reloadData];
+            [self saveData];
 
         }];
         
-        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Task done" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){
+        UIAlertAction *doneAction = [UIAlertAction actionWithTitle:@"Task done" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){
+           
             
             
+           // NSString *string = [NSString stringWithFormat:@"%ld", (long)selectedCell.row];
+           
+            NSString *string = self.todoTask[selectedCell.row];
+            NSString *string1 = self.importantTask[selectedCell.row];
+                //[self.todoTask removeObjectAtIndex:selectedCell.row];
+              //  [self.completedTask addObject:string];
             
-            NSMutableString *d = self.todoTask[selectedCell.row];
-                              
-                     [self.todoTask removeObjectAtIndex:selectedCell.row];
-                     [self.todoTask insertObject:d atIndex:self.todoTask.count];
-                    
-                     [tableView reloadData];
+           
             
+            NSLog(@"SELECTED THE CELL? %d", (long)selectedCell.section == 0);
             
+            if(selectedCell.section == 0){
+               [self.importantTask removeObjectAtIndex:selectedCell.row];
+                [self.completedTask addObject:string1];
+            }else if(selectedCell.section == 1){
+               [self.todoTask removeObjectAtIndex:selectedCell.row];
+                [self.completedTask addObject:string];
+            }else if(selectedCell.section == 2){
+               [self.completedTask removeObjectAtIndex:selectedCell.row];
+                
+            }
+           
+        
             NSLog(@"Task done");
             
+            [self.tableView reloadData];
             [self saveData];
             
         }];
         
         UIAlertAction *deleteAction = [UIAlertAction actionWithTitle:@"Delete" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){
-            NSLog(@"Task done");
             
-            if(selectedCell.row == UITableViewCellAccessoryCheckmark){
-                [tableView cellForRowAtIndexPath:selectedCell].accessoryType = UITableViewCellAccessoryNone;
+            
+            
+            if(selectedCell.section == 0){
+                [self.importantTask removeObjectAtIndex:selectedCell.row];
+            }else if(selectedCell.section == 1){
+                [self.todoTask removeObjectAtIndex:selectedCell.row];
+            }else if(selectedCell.section == 2){
+                [self.completedTask removeObjectAtIndex:selectedCell.row];
             }
             
-            [self.todoTask removeObjectAtIndex:selectedCell.row];
             [self.tableView reloadData];
             [self saveData];
             
-        
         }];
+        
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){
+            
+            }];
+        
         
         [alert addAction:prioAction];
                                        
-        [alert addAction:cancelAction];
+        [alert addAction:doneAction];
         
         [alert addAction:deleteAction];
         
+        [alert addAction:cancelAction];
         
         [self presentViewController:alert animated:YES completion:nil];
         
     }
 }
 }
-
-
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 
-    return 1;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    
+    if (section == 0) {
+        return self.importantTask.count;
+    }
 
+     if (section == 1) {
+        return self.todoTask.count;
+    }
+
+     if (section == 2) {
+        return self.completedTask.count;
+    }else {
+        return 0;
+    }
    
-    return self.todoTask.count;
 }
-
-
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"todoCell" forIndexPath:indexPath];
     
-    NSMutableArray *picForPrio = self.todoTask[indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"todoCell"];
+      
+      if(!cell)
+      {
+          cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"todoCell"];
+      }
     
-    cell.detailTextLabel.text = @"❗️";
-    
-    NSLog(@"%@", picForPrio);
-    
-    
-        NSMutableArray *todos = self.todoTask[indexPath.row];
-       
-       cell.textLabel.text = [NSString stringWithFormat:@"%@", todos];
-    
-       
-       NSLog(@"%@", todos);
     
 
+    if(indexPath.section == 0){
+        
+         NSMutableString *prio = [[NSMutableString alloc]initWithString:self.importantTask[indexPath.row]];
+           
+        cell.textLabel.text = [NSString stringWithString:prio];
+        
+           NSLog(@"cellForRowIMPORTANT: %@", self.importantTask);
+        
+        
+    } if(indexPath.section == 1){
     
+                 NSMutableString *todo = [[NSMutableString alloc]initWithString:self.todoTask[indexPath.row]];
+                    
+                 cell.textLabel.text = [NSString stringWithString:todo];
+              
+            NSLog(@"cellForRowTODO: %@", self.todoTask);
+        
+    }if(indexPath.section == 2) {
+    
+         NSMutableString *comp = [[NSMutableString alloc]initWithString:self.completedTask[indexPath.row]];
+           
+        cell.textLabel.text = [NSString stringWithString:comp];
+     
 
-    
+       NSLog(@"cellForRowCOMP: %@", self.completedTask);
+
+    }
     return cell;
 }
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    
+    switch (section) {
+    case 0:
+        return @"Important";
+        break;
+    case 1:
+        return @"Todos";
+        break;
+    case 2:
+        return @"Completed";
+        break;
 
+    default:
+        return nil;
+        break;
+        }
+    
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    if ([tableView.dataSource tableView:tableView numberOfRowsInSection:section] == 0) {
+        return 0;
+    } else {
+        return 50;
+    }
+}
 
 /*
 // Override to support conditional editing of the table view.
